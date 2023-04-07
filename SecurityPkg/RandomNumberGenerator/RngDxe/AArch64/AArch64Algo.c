aaab39ef82a40d252d6cedc8591ed05ce5690fdb
Pierre Gondois <pierre.gondois@arm.com>
2023-04-07 11:22:37 +0200

    SecurityPkg/RngDxe: Use GetRngGuid() when probing RngLib
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=4151
    
    The EFI_RNG_PROTOCOL can rely on the RngLib. The RngLib has multiple
    implementations, some of them are unsafe (e.g. BaseRngLibTimerLib).
    To allow the RngDxe to detect when such implementation is used,
    a GetRngGuid() function was added in a previous patch.
    
    The EFI_RNG_PROTOCOL can advertise multiple algorithms through
    Guids. The PcdCpuRngSupportedAlgorithm is currently used to
    advertise the RngLib in the Arm implementation.
    
    The issues of doing that are:
    - the RngLib implementation might not use CPU instructions,
      cf. the BaseRngLibTimerLib
    - most platforms don't set PcdCpuRngSupportedAlgorithm
    
    A GetRngGuid() was added to the RngLib in a previous patch,
    allowing to identify the algorithm implemented by the RngLib.
    Make use of this function.
    
    Signed-off-by: Pierre Gondois <pierre.gondois@arm.com>

--- SecurityPkg/RandomNumberGenerator/RngDxe/AArch64/AArch64Algo.c
+++ SecurityPkg/RandomNumberGenerator/RngDxe/AArch64/AArch64Algo.c
@@ -10,6 +10,7 @@
 #include <Library/DebugLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/ArmTrngLib.h>
+#include <Library/RngLib.h>
 
 #include "RngDxeInternals.h"
 
@@ -28,9 +29,13 @@ GetAvailableAlgorithms (
   VOID
   )
 {
-  UINT64  DummyRand;
-  UINT16  MajorRevision;
-  UINT16  MinorRevision;
+  EFI_STATUS  Status;
+  UINT16      MajorRevision;
+  UINT16      MinorRevision;
+  GUID        RngGuid;
+  BOOLEAN     UnSafeAlgo = FALSE;
+
+  mAvailableAlgoArrayCount = 0;
 
   // Rng algorithms 2 times, one for the allocation, one to populate.
   mAvailableAlgoArray = AllocateZeroPool (RNG_AVAILABLE_ALGO_MAX);
@@ -38,24 +43,46 @@ GetAvailableAlgorithms (
     return EFI_OUT_OF_RESOURCES;
   }
 
-  // Check RngGetBytes() before advertising PcdCpuRngSupportedAlgorithm.
-  if (!EFI_ERROR (RngGetBytes (sizeof (DummyRand), (UINT8 *)&DummyRand))) {
+  // Identify RngLib algorithm.
+  Status = GetRngGuid (&RngGuid);
+  if (!EFI_ERROR (Status)) {
+    if ((IsZeroGuid (&RngGuid)) ||
+        (CompareGuid (&RngGuid, &gEfiRngAlgorithmUnSafe))) {
+      // Treat zero GUID as an unsafe algorithm
+      DEBUG ((
+        DEBUG_WARN,
+        "RngLib uses an Unsafe algorithm and "
+        "must not be used for production builds.\n"
+        ));
+      // Set the UnSafeAlgo flag to indicate an unsafe algorithm was found
+      // so that it can be added at the end of the algorithm list.
+      UnSafeAlgo = TRUE;
+    } else {
+      CopyMem (
+        &mAvailableAlgoArray[mAvailableAlgoArrayCount],
+        &RngGuid,
+        sizeof (RngGuid)
+        );
+      mAvailableAlgoArrayCount++;
+    }
+  }
+  // Raw algorithm (Trng)
+  if (!EFI_ERROR (GetArmTrngVersion (&MajorRevision, &MinorRevision))) {
     CopyMem (
       &mAvailableAlgoArray[mAvailableAlgoArrayCount],
-      PcdGetPtr (PcdCpuRngSupportedAlgorithm),
+      &gEfiRngAlgorithmRaw,
       sizeof (EFI_RNG_ALGORITHM)
       );
     mAvailableAlgoArrayCount++;
-
-    DEBUG_CODE_BEGIN ();
-    if (IsZeroGuid (PcdGetPtr (PcdCpuRngSupportedAlgorithm))) {
-      DEBUG ((
-        DEBUG_WARN,
-        "PcdCpuRngSupportedAlgorithm should be a non-zero GUID\n"
-        ));
-    }
-
-    DEBUG_CODE_END ();
+  }
+  // Add unsafe algorithms at the end of the list.
+  if (UnSafeAlgo) {
+    CopyMem (
+      &mAvailableAlgoArray[mAvailableAlgoArrayCount],
+      &gEfiRngAlgorithmUnSafe,
+      sizeof (EFI_RNG_ALGORITHM)
+      );
+    mAvailableAlgoArrayCount++;
   }
 
   // Raw algorithm (Trng)
